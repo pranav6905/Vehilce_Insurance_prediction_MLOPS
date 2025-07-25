5c992277e5cc3739f8d2b85447d4304cee2ac835
pranav6905 <pranavkhunt6905@gmail.com>
2025-07-25 15:13:11 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Pranav Khunt",
+    author_email="pranavkhunt6905@gmail.com",
+    packages=find_packages()
+)
